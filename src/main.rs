0ebbe2c8b5a7a0bb2ad6ab8835631e7eb717b8e7
Max Brown <max.brown@ed.ac.uk>
2023-10-05 17:46:27 +0100

    More families added; reorganise code base slightly.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 /// An implementation of floral formulas
 /// following Judd et al., 2002.
-use floral::parse_args;
+use floral::cli::parse_args;
 
 fn main() -> std::result::Result<(), Box<dyn std::error::Error>> {
     match parse_args() {
